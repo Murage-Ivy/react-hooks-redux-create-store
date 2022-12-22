29f100ef24d6c5ba920474301a883357a11b76ad
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-12-22 14:01:09 +0300

    Completed assignment

--- src/createStore.js
+++ src/createStore.js
@@ -1,28 +1,48 @@
-let state;
+function createStore(reducer) {
+  let state;
 
-function reducer(state = { count: 0 }, action) {
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  return {
+    dispatch,
+    getState
+  };
+}
+
+function reducer(state = {
+  count: 0
+}, action) {
   switch (action.type) {
     case "counter/increment":
-      return { count: state.count + 1 };
+      return {
+        count: state.count + 1
+      };
 
     default:
       return state;
   }
 }
 
-function dispatch(action) {
-  state = reducer(state, action);
-  render();
-}
-
 function render() {
   let container = document.getElementById("container");
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 }
+let store = createStore()
+store.dispatch({
+  type: "@@INIT"
+})
 
-dispatch({ type: "@@INIT" });
 let button = document.getElementById("button");
 
 button.addEventListener("click", function () {
-  dispatch({ type: "counter/increment" });
+  store.dispatch({
+    type: "counter/increment"
+  });
 });
